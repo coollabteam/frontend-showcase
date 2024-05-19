d69de983bc037c9ae766ece8f569b180ea433d01
Ishin Vin <ishinvin26@gmail.com>
2024-05-19 23:44:50 +0700

    Updates

---
+++ _nuxt/static/1716137085/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcoollabteam.github.io\u002F_nuxt\u002Fstatic\u002F1716137085",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcoollabteam.github.io\u002F",assetsPath:"\u002Fcoollabteam.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
